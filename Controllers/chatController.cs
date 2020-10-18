56a27b6965bef21b8008b7537410ee1e0cfdb1bc
Rusakov Aleksey <rusal@bk.ru>
2020-10-18 22:25:30 +0300

    Добавьте файлы проекта.

---
+++ Controllers/chatController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace chat_server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class chatController : ControllerBase
+    {
+        public static MessagesClass ms = new MessagesClass();
+        //// GET: api/<chatController>
+        //[HttpGet]
+        //public IEnumerable<string> Get()
+        //{
+        //    return new string[] { "Udachi na kontrolnoy u Kashkina" };
+        //}
+
+        // GET api/<chatController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            string json = " not found";
+            if ((id < ms.messages.Count) && (id>=0))
+            {
+                json = JsonSerializer.Serialize(ms.messages.ElementAt(id));
+            }
+            return json;
+        }
+
+        // POST api/<chatController>
+        [HttpPost]
+        public void Post([FromBody] message msg)
+        {
+            ms.Add(msg.username, msg.text);
+        }
+
+        //// PUT api/<chatController>/5
+        //[HttpPut("{id}")]
+        //public void Put(int id, [FromBody] string value)
+        //{
+        //}
+
+        //// DELETE api/<chatController>/5
+        //[HttpDelete("{id}")]
+        //public void Delete(int id)
+        //{
+        //}
+    }
+}
---
+++ MessagesClass.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace chat_server
+{
+    [Serializable]
+    public class message
+    {
+        public message(string username, string text, DateTime timestamp)
+        {
+            this.username = username;
+            this.text = text;
+            this.timestamp = timestamp;
+        }
+        public message()
+        {
+            this.username = "Server";
+            this.text = "Server is running";
+            this.timestamp = DateTime.UtcNow;
+        }
+        public message(string username, string text)
+        {
+            this.username = username;
+            this.text = text;
+            this.timestamp = DateTime.UtcNow;
+        }
+        public string username { get; set; }
+        public string text { get; set; }
+        public DateTime timestamp { get; set; }
+    }
+    [Serializable]
+    public class MessagesClass
+    {
+        public  List<message> messages = new List<message>();
+
+        public MessagesClass(List<message> messages)
+        {
+            messages.Clear();
+            message ms = new message();
+            messages.Add(ms);
+        }
+
+        public MessagesClass()
+        {
+            messages.Clear();
+            message ms = new message();
+            messages.Add(ms);
+        }
+
+        public void Add(string username, string text)
+        {
+            message msg = new message(username, text);
+            messages.Add(msg);
+            Console.WriteLine($"Сообщение: {text}  номер {messages.Count}");
+        }
+
+        public void Add(message msg)
+        {
+            msg.timestamp = DateTime.UtcNow;
+            messages.Add(msg);
+            Console.WriteLine($"Сообщений:  {messages.Count}");
+        }
+
+    }
+}
